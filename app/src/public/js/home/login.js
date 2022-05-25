fe5b48c002d6948d95535ac743e6da134690d26e
hiseo <shi@codeblock.co.kr>
2022-05-25 15:09:57 +0900

    nodemon 설치 & DOM 이용해서 html 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,1 +1,17 @@
 "use strict";
+
+// 문서객체 모델인 DOM 인터페이스를 사용해서 html에 존재하는 데이터를 가져와서 제어할 수 있다.
+// DOM -> Document Object Mdoel
+
+const id = document.querySelector("#id"),
+pswrod = document.querySelector("#psword"),
+loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click",login());
+
+function login(){
+    const req = {
+        id : id.value,
+        psword: pswrod.value,
+    };
+};
