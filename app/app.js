f0b691bccbe82b3778c32ce3733bedafe6a8b066
hiseo <shi@codeblock.co.kr>
2022-05-25 14:38:00 +0900

    git 파일 수정

--- app/app.js
+++ app/app.js
@@ -11,6 +11,11 @@ const home = require('./src/routes/home') //만들어준 파일이기 때문에 
 app.set("views","./src/views"); //view를 어디서 찾는지
 app.set("view engine","ejs") //동작할 코드들을 어떤 engine으로 해석할지 설정 
 
-app.use("/", home); //use -> 미들웨어를 등록해주는 메서드 ( "/" 경로로 오면 home으로 이동 > home으로 가서 라우터가 주소 찾아서 callback )
+// login.ejs에 login.js로 접속할 수 있게 정적경로를 추가
+// dirname:현재 있는 파일의 경로를 반환 > 그 안에 있는 src/public 폴더를 정적경로로 추가
+app.use(express.static('${__dirname}/src/public'));
+
+//use -> 미들웨어를 등록해주는 메서드 ( "/" 경로로 오면 home으로 이동 > home으로 가서 라우터가 주소 찾아서 callback )
+app.use("/", home); 
 
 module.exports = app;
