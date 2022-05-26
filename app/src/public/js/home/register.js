68200389affd8d0c4b18cb2005b4d521a5014e41
hiseo <shi@codeblock.co.kr>
2022-05-26 10:02:39 +0900

    회원가입 요청 구현

---
+++ app/src/public/js/home/register.js
@@ -0,0 +1,40 @@
+"use strict";
+
+// 문서객체 모델인 DOM 인터페이스를 사용해서 html에 존재하는 데이터를 가져와서 제어할 수 있다.
+// DOM -> Document Object Mdoel
+
+const id = document.querySelector("#id"),
+    name = document.querySelector("#name"),
+    psword = document.querySelector("#psword"),
+    confirmPsword = document.querySelector("#confirm-psword"),
+    registerBtn = document.querySelector("#button");
+
+registerBtn.addEventListener("click",register);
+
+function register(){
+    const req = {
+        id : id.value,
+        name : name.value,
+        psword: psword.value,
+        confirmPsword: confirmPsword.value,  
+    };
+   
+    fetch("/register", {
+        method: "POST",
+        headers:{
+            "Content-Type" : "application/json",
+        },
+        body : JSON.stringify(req),
+    })
+    .then((res) => res.json())
+    .then((res) => {
+        if(res.success){
+            location.href = "/login";
+        }else{
+            alert(res.msg);
+        }
+    }).catch((err) => {
+        console.error("회원가입 중 에러 발생");
+    });
+   
+}
